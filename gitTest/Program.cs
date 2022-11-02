4f57c1caca44f854dfe527fe07e23834792e4551
Miloš Ljubisavljević <milos.ljubisavljevic@fon.bg.ac.rs>
2022-11-02 12:50:26 +0100

    dodat unos sa konzole

--- gitTest/Program.cs
+++ gitTest/Program.cs
@@ -1,2 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+Console.WriteLine("dodat jos jedan console log");
+
+
+
+var a = Console.ReadLine();
+Console.WriteLine(a);
